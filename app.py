d621a6ba4f33264bee83e99d1ed40f4235c6afde
dmorenoderm-rgb <dmorenoderm@gmail.com>
2026-03-24 14:46:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ solicitantes = [
 ]
 
 protocolos = {
-    "Seleccionar"
+    "Seleccionar",
     "Psoriasis en placas": [
         "Adalimumab 40 mg/2 semanas",
         "Ustekinumab 45 mg/12 semanas",
