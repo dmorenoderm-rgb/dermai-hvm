848643c91917b7740234096e75fbeba41ba63f5b
dmorenoderm-rgb <dmorenoderm@gmail.com>
2026-03-24 13:09:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -50,19 +50,20 @@ if st.sidebar.button("Cerrar sesión"):
 conn = sqlite3.connect("data.db")
 c = conn.cursor()
 
-c.execute(
-    "CREATE TABLE requests ("
-    "id TEXT PRIMARY KEY,"
-    "paciente TEXT,"
-    "solicitante TEXT,"
-    "enfermedad TEXT,"
-    "tratamiento TEXT,"
-    "estado TEXT,"
-    "comentario TEXT,"
-    "fecha TEXT,"
-    "fecha_director TEXT,"
-    "fecha_farmacia TEXT)"
+c.execute("""
+CREATE TABLE IF NOT EXISTS requests (
+    id TEXT PRIMARY KEY,
+    paciente TEXT,
+    solicitante TEXT,
+    enfermedad TEXT,
+    tratamiento TEXT,
+    estado TEXT,
+    comentario TEXT,
+    fecha TEXT,
+    fecha_director TEXT,
+    fecha_farmacia TEXT
 )
+""")
 conn.commit()
 
 # ======================
